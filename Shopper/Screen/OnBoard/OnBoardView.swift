d5e608ea4f13bee9573a411720e6c8de548fa2e4
saengle <zohv817@gmail.com>
2024-06-13 20:29:05 +0900

    [Add] OnBoardScreen Files

---
+++ Shopper/Screen/OnBoard/OnBoardView.swift
@@ -0,0 +1,19 @@
+//
+//  OnBoardView.swift
+//  Shopper
+//
+//  Created by 쌩 on 6/13/24.
+//
+
+import UIKit
+
+class OnBoardView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Shopper/Screen/OnBoard/OnBoardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardViewController.swift
+//  Shopper
+//
+//  Created by 쌩 on 6/13/24.
+//
+
+import UIKit
+
+class OnBoardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
