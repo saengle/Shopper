0041fb6a70a4105d75551f946dc5d2aadd2dacc4
saengle <zohv817@gmail.com>
2024-06-13 20:25:46 +0900

    [Feat] Launch Screen Add MyColors

--- Shopper/AppDelegate.swift
+++ Shopper/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // splash sleep 2초
+//        sleep(2)
         return true
     }
 
---
+++ Shopper/Resource/SColors.swift
@@ -0,0 +1,17 @@
+//
+//  SColors.swift
+//  Shopper
+//
+//  Created by 쌩 on 6/13/24.
+//
+
+import UIKit
+
+enum MyColors {
+    static let primary = #colorLiteral(red: 0.9564719796, green: 0.610019803, blue: 0.3464771509, alpha: 1)
+    static let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    static let gray = #colorLiteral(red: 0.5815095305, green: 0.5815094709, blue: 0.5815094709, alpha: 1)
+    static let darkGray = #colorLiteral(red: 0.3705568314, green: 0.3705568314, blue: 0.3705568314, alpha: 1)
+    static let lightGray = #colorLiteral(red: 0.8407003284, green: 0.8407003284, blue: 0.8407003284, alpha: 1)
+    static let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+}
